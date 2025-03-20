e82c1be12eb47c4683adde06cdc8b63459f575f9
mulakalaashritha <153161182+mulakalaashritha@users.noreply.github.com>
2025-03-20 19:25:51 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-# Configure the AWS Provider
+
 provider "aws" {
   
-  region = "us-east-1"
+region = "us-east-1"
 
 }
 
-# instance creation 
-resource "aws_instance" "Prod" {
-  
- ami = "ami-053a45fff0a704a47"
- instance_type = "t2.micro"
- key_name = "madhukey"
- count = 1
 
- tags = {
-    Name = "Prod"
- }
+resource "aws_instance" "test" {
   
+  ami = "ami-053a45fff0a704a47"
+  instance_type = "t2.micro"
+  key_name = "ramanikey"
+   count = 1
+
+  tags = {
+    Name = "LAB"
+  }
+
 }
